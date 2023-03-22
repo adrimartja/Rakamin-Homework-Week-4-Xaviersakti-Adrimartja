d0e2afbe5d1a62f026ca41d8198f80d8573b056a
adrimartja <adrimartja@gmail.com>
2023-03-22 19:07:18 +0700

    menukar susunan min and max

--- script.js
+++ script.js
@@ -75,22 +75,6 @@ console.log(`Rata-rata Index Ganjil = ${average(bilanganIndexGanjil)}`);
 console.log(`Rata-rata Index Genap = ${average(bilanganIndexGenap)}`);
 
 console.log("----------------------------------------");
-//determining max number
-function maxNumber(array) {
-  let max = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-  return max;
-}
-
-console.log(`Max Bilangan Utama = ${maxNumber(bilanganUtama)}`);
-console.log(`Max Index Ganjil = ${maxNumber(bilanganIndexGanjil)}`);
-console.log(`Max Index Genap = ${maxNumber(bilanganIndexGenap)}`);
-
-console.log("----------------------------------------");
 //determining min number
 function minNumber(array) {
   let min = array[0];
@@ -105,6 +89,23 @@ function minNumber(array) {
 console.log(`Min Bilangan Utama = ${minNumber(bilanganUtama)}`);
 console.log(`Min Index Ganjil = ${minNumber(bilanganIndexGanjil)}`);
 console.log(`Min Index Genap = ${minNumber(bilanganIndexGenap)}`);
+
+
+console.log("----------------------------------------");
+//determining max number
+function maxNumber(array) {
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+
+console.log(`Max Bilangan Utama = ${maxNumber(bilanganUtama)}`);
+console.log(`Max Index Ganjil = ${maxNumber(bilanganIndexGanjil)}`);
+console.log(`Max Index Genap = ${maxNumber(bilanganIndexGenap)}`);
 
 console.log("----------------------------------------");
 //creating function for comparing the value between odd and even index of the array.
